ee755f3235b905e748ed98a4d37b1b6ba1949d58
alreyy ☆ <loveformeemmll@gmail.com>
2025-11-22 07:39:06 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 global.owner = ['6283165995953', '12364167796'] // wajib di isi tidak boleh kosong
 global.mods  = ['6283165995953', '12364167796'] // wajib di isi tidak boleh kosong
 global.prems = ['6283165995953', '12364167796'] // wajib di isi tidak boleh kosong
-global.nameowner = 'Tio' // wajib di isi tidak boleh kosong
+global.nameowner = 'alreyy' // wajib di isi tidak boleh kosong
 global.numberowner = '6283165995953' // wajib di isi tidak boleh kosong
 global.mail = 'xxiissmm@kakao.com' // wajib di isi tidak boleh kosong
-global.gc = '' // wajib di isi tidak boleh kosong
+global.gc = 'https://instagram.com/mail.php' // wajib di isi tidak boleh kosong
 global.instagram = 'https://instagram.com/mail.php' // wajib di isi tidak boleh kosong
 global.wm = '© JISOO' // isi nama bot atau nama kalian
 global.wait = '_*Tunggu sedang di proses...*_' // ini pesan simulasi loading
